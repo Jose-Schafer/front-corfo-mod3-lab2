b0d4175f75c614d805a140e496f6cb62420f79c1
Jose Schafer <joseignacio.schafer@gmail.com>
2024-12-04 08:38:24 -0300

    Implementa clases de Doctor

--- src/js/components/doctors/html-builder.js
+++ src/js/components/doctors/html-builder.js
@@ -1,4 +1,14 @@
+import { GeneralDoctor, Surgeon } from './models';
+
 export function createDoctorHTML(image, name, specialty, titulo, magister, experience) {
+
+  let doctor;
+  if (specialty === "Cirujano") {
+    doctor = new Surgeon(name, specialty, experience);
+  } else {
+    doctor = new GeneralDoctor(name, specialty, experience);
+  }
+
   const card = document.createElement('div');
   card.className = 'card col-8 col-lg-3 m-lg-2 mx-auto';
 
@@ -11,12 +21,12 @@ export function createDoctorHTML(image, name, specialty, titulo, magister, exper
 
   const h5 = document.createElement("h5");
   h5.className = 'card-title';
-  h5.textContent = name;
+  h5.textContent = doctor.name;
   textDiv.appendChild(h5);
 
   const p = document.createElement('p');
   p.className = 'card-description';
-  p.textContent = `${specialty} (${experience})`;
+  p.textContent = `${doctor.showInformation()}`;
   textDiv.appendChild(p);
 
   const p2 = document.createElement('p');
@@ -28,6 +38,11 @@ export function createDoctorHTML(image, name, specialty, titulo, magister, exper
   p3.className = 'card-description';
   p3.textContent = `Magister: ${magister}`;
   textDiv.appendChild(p3);
+
+  const p4 = document.createElement('p');
+  p4.className = 'card-description';
+  p4.textContent = doctor.getDoctorMetrics();
+  textDiv.appendChild(p4);
 
   card.appendChild(img);
   card.appendChild(textDiv);
